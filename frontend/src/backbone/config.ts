e9ad48eedd589d7ec067bad59ea5181e6fca8e36
Pave1Pal <pd271828@gmail.com>
2024-09-14 17:36:49 +0300

    feate: add backend

---
+++ frontend/src/backbone/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  serverAppOrigin: new URL(import.meta.env.VITE_SERVER_APP_ORIGIN)
+} as const;
--- src/backbone/config.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  contractAddress: import.meta.env.VITE_CONTRACT_ADDRESS,
-  zCredFrontendUrl: new URL(import.meta.env.VITE_ZCRED_FRONTEND_URL),
-  zCredStoreSecretDataRouteUrl: new URL(import.meta.env.VITE_ZCRED_STORE_SECRET_DATA_ROUTE_URL),
-} as const;
