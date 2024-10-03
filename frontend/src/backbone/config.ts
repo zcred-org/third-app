ca986ec11a7a3df2711aa3df0a0a67790247bfe1
Biryukov Ilya <bis21080000@gmail.com>
2024-10-03 13:16:30 +0300

    ops(frontend): add env.VITE_WALLET_CONNECT_PROJECT_ID

--- frontend/src/backbone/config.ts
+++ frontend/src/backbone/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   isDev: import.meta.env.DEV,
   isProd: import.meta.env.PROD,
-  serverAppOrigin: new URL(import.meta.env.VITE_SERVER_APP_ORIGIN)
+  serverAppOrigin: new URL(import.meta.env.VITE_SERVER_APP_ORIGIN),
+  walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID as string,
 } as const;
