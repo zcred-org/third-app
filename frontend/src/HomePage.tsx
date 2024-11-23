b7def4dda5540c2aaf2ccd206db5b3e8c465bda5
Biryukov Ilya <bis21080000@gmail.com>
2024-11-23 23:13:06 +0300

    fix(frontend): wine was stretched on high screen, panel was above menu

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -22,7 +22,7 @@ function MainSection() {
   const verification = useVerification();
   const isVerified = verification.data?.isVerified;
 
-  const wineClassName = 'h-full w-auto transition duration-1000 xl:h-[100%] xl:absolute xl:left-1/4 xl:-translate-x-1/2 xl:bottom-[-15%]';
+  const wineClassName = 'z-[-1] h-full transition duration-1000 xl:h-[100%] xl:absolute xl:left-1/4 xl:-translate-x-1/2 xl:bottom-[-15%]';
 
   const buttonProps: ButtonProps<'a'> = !wallet.isConnected ? {
     children: (<>
@@ -43,14 +43,14 @@ function MainSection() {
 
   return (
     <main className="flex flex-col gap-3 grow justify-center items-center pt-5 pb-10 xl:ml-[25%] xl:gap-14">
-      <div className="grow h-0 w-auto min-h-[400px] relative xl:absolute xl:h-[unset] xl:inset-0 pointer-events-none">
+      <div className="grow h-0 max-xl:w-full flex justify-center min-h-[400px] relative xl:absolute xl:h-[unset] xl:inset-0 pointer-events-none">
         <img
           src="/images/bottle_from_with_korob_shadow.png" alt=""
           className={cn(wineClassName, isVerified ? 'opacity-100' : 'opacity-0')}
         />
         <img
           src="/images/korob_shadow.png" alt=""
-          className={cn(wineClassName, 'ml-3 xl:ml-2 absolute top-0 xl:top-[unset] left-0', isVerified ? 'opacity-0 -translate-y-20' : 'opacity-100')}
+          className={cn(wineClassName, 'ml-3 xl:ml-2 absolute top-0 xl:top-[unset]', isVerified ? 'opacity-0 -translate-y-20' : 'opacity-100')}
         />
       </div>
       <h1 className="font-serif text-center text-2xl xl:text-5xl text-white font-bold">
--- frontend/src/compontents/UnverifiedPanel.tsx
+++ frontend/src/compontents/UnverifiedPanel.tsx
@@ -9,13 +9,13 @@ export function UnverifiedPanel() {
   return (
     <Transition show={!verification.data?.isVerified}>
       <div className={`
-            group sticky top-0 overflow-hidden z-[1]
+            group sticky top-0 overflow-hidden
             py-1 px-5 max-xl:w-full backdrop-blur-lg
             text-orange-100 bg-orange-400/30 border-b border-orange-300/30
             transition-all duration-300 data-[closed]:py-0 data-[closed]:opacity-0`}
       >
         <div className="flex items-center justify-center gap-3 text-base transition-all duration-300 group-data-[closed]:[line-height:0px]">
-          <IconUnverified className="shrink-0 size-5 -mb-1 transition-all duration-300 group-data-[closed]:h-0" />
+          <IconUnverified className="shrink-0 size-6 -mb-1 transition-all duration-300 group-data-[closed]:h-0" />
           To continue shopping you need prove that you are adult and not from USA
         </div>
       </div>
